84676aa13d1ba730a199a2de7ae7a6c63d37fe98
nikhil <nikhilsaxena9811839995@gmail.com>
2019-02-16 01:31:59 +0530

    new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatListModule } from '@angular/material/list';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { BrowserModule } from '@angular/platform-browser';
@@ -8,17 +9,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule, 
     BrowserAnimationsModule,
     MatToolbarModule,
-    FlexLayoutModule
-
+    FlexLayoutModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/dish.ts
@@ -0,0 +1,10 @@
+export class Dish {
+	id: string;
+	name: string;
+	image: string;
+	category: string;
+	featured: boolean;
+	label: string;
+	prise: string;
+	description: string;
+}
